dc0b7379bc8650a2ea2642fcfcc3ca612f80c236
devstar <dev_star11@outlook.com>
2019-01-16 22:34:52 +1000

    add list, start, stop backend apis

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var cors = require('cors');
+var bodyparser = require('body-parser');
+var engines = require('consolidate');
+
+var router = express.Router();
+var app = express(); 
+var server = require('http').Server(app);
+var port = 80;
+
+app.engine('html', engines.hogan);
+app.set('view engine', 'html');
+app.use(cors());
+
+// body-parser
+app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.json());
+
+app.use('/', express.static('public'))
+
+var routes = require('./routes')(router);
+
+app.use('/', routes);
+
+server.listen(port, function(){
+  console.log("Node.js docker management server  running %d port", port);
+});
